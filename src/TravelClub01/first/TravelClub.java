e1ae1dafa9815a1badda65005e588bb4191a31e8
kkamjjing2 <kkamjjing2@gmail.com>
2024-08-05 15:26:32 +0900

    [feat] stage01 28페이지-여행 클럽 클래스 만들기

---
+++ src/TravelClub01/first/TravelClub.java
@@ -0,0 +1,10 @@
+package TravelClub01.first;
+
+// 여행 클럽 클래스 만들기
+// 필드는 name과 intro
+// 필드의 접근 지정자를 private 로 지정해야 하는 이유 :
+// 필드의 타입을 String 으로 정한 이유 :
+public class TravelClub {
+    private String name;
+    private String intro;
+}
